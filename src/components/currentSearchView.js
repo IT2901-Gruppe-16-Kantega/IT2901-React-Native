70698cc07b889f68077f38f9a210bf285fc30a32
Sontha <sondretharaldsen@gmail.com>
2017-03-13 14:56:29 +0100

    Added functionality to change to AR app and added ios appicons

--- src/components/currentSearchView.js
+++ src/components/currentSearchView.js
@@ -4,7 +4,8 @@ import {
   View,
   Text,
   StyleSheet,
-  TouchableHighlight
+  TouchableHighlight,
+  Linking
 
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
@@ -93,6 +94,7 @@ var CurrentSearchView = React.createClass({
 
     //kan brukes ved mottak av data fra unity
     //this.props.fetchDataReturned(objects, true);
+    Linking.openURL("ARApp:");
 
   },
   exit() {
